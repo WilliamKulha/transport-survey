167416dbd41f81d0bb1892521e9decf392aaf0e5
William Kulha <kulha.william@gmail.com>
2018-10-01 14:01:12 -0400

    Add scripts to capture user input and then display it in it's span div when the form is submitted for fun transportation.

--- js/scripts.js
+++ js/scripts.js
@@ -7,6 +7,11 @@ $(document).ready(function() {
     let workTransportationMode = $(this).val();
     $('#work-responses').append(workTransportationMode + '<br>');
   });
+  $('#fun-responses').show();
+  $('input:checkbox[name=fun-transportation]:checked').each(function() {
+    let funTransportationMode = $(this).val();
+    $('#fun-responses').append(funTransportationMode + '<br>');
+  });
   $('#transportation_survey').hide();
   });
 });
